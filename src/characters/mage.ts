ce2e3c8ad18f93dd321f286432e673e294ecad95
kaori takahashi <anohitohaima@ymobile.ne.jp>
2026-07-01 16:40:02 +0900

    Mageの攻撃力を設定

--- src/characters/mage.ts
+++ src/characters/mage.ts
@@ -1,7 +1,13 @@
-import { Character } from './character.ts';
+import { Character } from "./character.ts";
 
 export class Mage extends Character {
-  override attack() {
+  constructor(name: string, hp: number) {
+    super(name, hp);
+    this.power = 40;
+  }
+
+  override attack(opponent: Character): void {
     console.log(`${this.name}は魔法を唱えた！`);
+    opponent.takeDamage(40);
   }
 }
